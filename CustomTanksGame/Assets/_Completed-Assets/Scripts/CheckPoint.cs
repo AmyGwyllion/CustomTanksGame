f3e471ba8675ef40c5413e18cca6ade75a1818a2
Amanda Oliver <amandaolivercalero@gmail.com>
2018-08-13 02:02:40 +0200

    Added Basic Elements

---
+++ CustomTanksGame/Assets/_Completed-Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+namespace Complete { 
+    public class CheckPoint : MonoBehaviour {
+
+        static Vector3 Reached;
+
+        private void OnTriggerEnter(Collider player)
+        {
+            Debug.Log(player.GetComponentInParent<TankMovement>().m_PlayerNumber);
+        }
+    }
+}
---
+++ CustomTanksGame/Assets/_Completed-Assets/Scripts/Tank/TankCompass.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankCompass : MonoBehaviour {
+
+    public Slider m_Slider;                             // The slider to represent how much health the tank currently has.
+    public Image m_FillImage;                           // The image component of the slider.
+    public Color m_Color = Color.green;                               // The arrow color
+    public Transform[] CheckPoints;                     // All the targets the 
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void PointToCheckPoint() {
+        //Here the arrow rotates to point the next checkpoint in the map
+    }
+}
