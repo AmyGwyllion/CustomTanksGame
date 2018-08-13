d0ca26f5727998e782dd671713d13b7f81c65c5b
Amanda Oliver <amandaolivercalero@gmail.com>
2018-08-13 02:22:30 +0200

    Compass Pointing To Checkpoints

--- CustomTanksGame/Assets/_Completed-Assets/Scripts/Tank/TankCompass.cs
+++ CustomTanksGame/Assets/_Completed-Assets/Scripts/Tank/TankCompass.cs
@@ -6,7 +6,7 @@ public class TankCompass : MonoBehaviour {
     public Slider m_Slider;                             // The slider to represent how much health the tank currently has.
     public Image m_FillImage;                           // The image component of the slider.
     public Color m_Color = Color.green;                               // The arrow color
-    public Transform[] CheckPoints;                     // All the targets the 
+    public Transform[] CheckPoints;                     // All the active checkpoints
 
     // Use this for initialization
     void Start () {
@@ -15,10 +15,21 @@ public class TankCompass : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        PointToCheckPoint();
 	}
 
     private void PointToCheckPoint() {
         //Here the arrow rotates to point the next checkpoint in the map
+        Vector3 to = CheckPoints[0].transform.position;
+        Vector3 from = m_Slider.transform.position;
+        Vector3 v = to - from;
+        v.Normalize();
+
+        float m_Angle = (Mathf.Atan2(v.x, v.z) * Mathf.Rad2Deg);
+
+        Vector3 rot = m_Slider.transform.eulerAngles;
+        Vector3 newRot = new Vector3(rot.x, m_Angle, rot.z);
+
+        m_Slider.transform.eulerAngles = newRot;
     }
 }
